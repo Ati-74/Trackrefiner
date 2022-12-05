d1472fe210c80287aee73573a6e9b736dbb238a6
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-12-05 07:55:35 +0330

    Update unit_test.py

--- unit test/unit_test.py
+++ unit test/unit_test.py
@@ -1,9 +1,23 @@
-import sys
-sys.path.append('../scripts/strain')
-from ProcessCellProfilerData import process_data
 
+from CellProfilerAnalysis.strain.ProcessCellProfilerData import process_data
 
 if __name__ == '__main__':
-    input_file = '../examples/SingleStrain/InputFile/single_strain_test.csv'
-    output_directory = '../examples/SingleStrain/outputs/'
-    process_data(input_file, output_directory)
+    input_file = '../examples/K12/K12_CP_output.csv'
+    # input_file = '../examples/test/IdentifyPrimaryObjects (copy).csv'
+    output_directory = '../examples/K12/outputs'
+    # output_directory = '../examples/test'
+
+    # optional parameters
+    # unit: minute
+    interval_time = 1.5
+    growth_rate_method = "Average"
+    # useful for fixing transition & more than two daughters errors
+    number_of_gap = 0
+    # convert pixel to um
+    um_per_pixel = 0.144
+    # for assign cell type
+    intensity_threshold = 0.1
+
+    # run post-processing
+    process_data(input_file, output_directory, interval_time, growth_rate_method, number_of_gap, um_per_pixel,
+                 intensity_threshold)
