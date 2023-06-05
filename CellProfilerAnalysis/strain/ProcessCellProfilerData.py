8e9dc784aa9d345e3663004fce8d94a022af84b5
Atiyeh Ahmadi <88287144+Ati-74@users.noreply.github.com>
2023-06-05 16:19:34 +0330

    object number has been added to the bacteria features list

--- CellProfilerAnalysis/strain/ProcessCellProfilerData.py
+++ CellProfilerAnalysis/strain/ProcessCellProfilerData.py
@@ -67,11 +67,12 @@ def create_pickle_files(df, path, assigning_cell_type):
         # select important columns
         if assigning_cell_type:
             data_frame_current_time_step = data_frame_current_time_step[
-                ['id', 'label', 'cellType', 'divideFlag', 'cellAge', 'growthRate', 'LifeHistory', 'startVol',
-                 'targetVol', 'pos', 'time', 'radius', 'length', 'dir', 'ends', 'strainRate', 'strainRate_rolling']]
+                ['ObjectNumber', 'id', 'label', 'cellType', 'divideFlag', 'cellAge', 'growthRate', 'LifeHistory',
+                 'startVol', 'targetVol', 'pos', 'time', 'radius', 'length', 'dir', 'ends', 'strainRate',
+                 'strainRate_rolling']]
         else:
             data_frame_current_time_step = data_frame_current_time_step[
-                ['id', 'label', 'divideFlag', 'cellAge', 'growthRate', 'LifeHistory', 'startVol',
+                ['ObjectNumber', 'id', 'label', 'divideFlag', 'cellAge', 'growthRate', 'LifeHistory', 'startVol',
                  'targetVol', 'pos', 'time', 'radius', 'length', 'dir', 'ends', 'strainRate', 'strainRate_rolling']]
         # convert to dictionary
         df_to_dict = data_frame_current_time_step.to_dict('index')
