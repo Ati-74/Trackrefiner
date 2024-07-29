b84a8a99638c5e0379304bf14ff5c5689e53e0f2
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2024-07-29 18:25:00 +0200

    minor changes

--- Trackrefiner/strain/correction/noiseRemover.py
+++ Trackrefiner/strain/correction/noiseRemover.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from Trackrefiner.strain.correction.action.findOutlier import find_bac_len_boundary
-from Trackrefiner.strain.correction.action.bacteriaModification import remove_bac
+from Trackrefiner.strain.correction.action.helperFunctions import remove_rows
 
 
 def remove_from_neighbors_df(neighbors_df, bad_obj):
@@ -26,17 +26,40 @@ def remove_from_neighbors_df(neighbors_df, bad_obj):
     return neighbors_df
 
 
-def noise_remover(df, neighbors_df, parent_image_number_col, parent_object_number_col, label_col,
-                  center_coordinate_columns, logs_df):
+def remove_noise_bac_info(df, noise_bac, parent_image_number_col, parent_object_number_col):
+
+    noise_obj_next_time_step = df.loc[(df[parent_image_number_col] == noise_bac['ImageNumber']) &
+                                      (df[parent_object_number_col] == noise_bac['ObjectNumber'])]
+
+    if noise_obj_next_time_step.shape[0] > 0:
+        df.loc[noise_obj_next_time_step.index.values, [parent_image_number_col, parent_object_number_col]] = [0, 0]
+
+    return df
+
+
+def noise_remover(df, neighbors_df, parent_image_number_col, parent_object_number_col, logs_df):
 
     num_noise_obj = None
+    noise_objects_log = []
+
+    detected_noise_bac_in_prev_time_step = df.loc[df['noise_bac']]
+
+    for noise_bac_ndx, noise_bac in detected_noise_bac_in_prev_time_step.iterrows():
+
+        df = remove_noise_bac_info(df, noise_bac, parent_image_number_col, parent_object_number_col)
+
+        logs_df = pd.concat([logs_df, df.iloc[noise_bac_ndx].to_frame().transpose()])
+
+        neighbors_df = remove_from_neighbors_df(neighbors_df, noise_bac)
+
+        noise_objects_log.append(str(noise_bac['ImageNumber']) + '\t' + str(noise_bac['ObjectNumber']))
 
     while num_noise_obj != 0:
 
         bac_len_boundary = find_bac_len_boundary(df)
 
         noise_objects_df = df.loc[(df['AreaShape_MajorAxisLength'] < bac_len_boundary['avg'] -
-                                   1.96 * bac_len_boundary['std']) & (df['noise_bac'] == False)]
+                                   1.96 * bac_len_boundary['std']) & (~ df['noise_bac'])]
 
         num_noise_obj = noise_objects_df.shape[0]
 
@@ -48,17 +71,16 @@ def noise_remover(df, neighbors_df, parent_image_number_col, parent_object_numbe
 
         for noise_bac_ndx, noise_bac in noise_objects_df.iterrows():
 
-            df = remove_bac(df, noise_bac_ndx, noise_bac, neighbors_df, parent_image_number_col,
-                            parent_object_number_col, label_col, center_coordinate_columns)
+            df = remove_noise_bac_info(df, noise_bac, parent_image_number_col, parent_object_number_col)
 
             df.at[noise_bac_ndx, 'noise_bac'] = True
 
-            logs_df = pd.concat([logs_df, df.iloc[noise_bac_ndx].to_frame().transpose()], ignore_index=True)
+            logs_df = pd.concat([logs_df, df.iloc[noise_bac_ndx].to_frame().transpose()])
 
             neighbors_df = remove_from_neighbors_df(neighbors_df, noise_bac)
 
             noise_objects_log.append(str(noise_bac['ImageNumber']) + '\t' + str(noise_bac['ObjectNumber']))
 
-        # df = remove_rows(df, 'noise_bac', False)
+    df = remove_rows(df, 'noise_bac', False)
 
     return df, neighbors_df, noise_objects_log, logs_df
