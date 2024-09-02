9392bfb2457ff605c7115e4c205bf628d79ad8d6
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2024-09-03 01:02:00 +0330

    Update cellProfilerBacteriaTrackingPlot.py

--- runningAnalysis/cellProfilerBacteriaTrackingPlot.py
+++ runningAnalysis/cellProfilerBacteriaTrackingPlot.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import cv2
 import os
+import shutil
+import argparse
+import glob
 
 
 def angle_convert_to_radian(df):
@@ -19,11 +22,31 @@ def angle_convert_to_radian(df):
     return df
 
 
-def bac_info(bac_in_timestep):
-    """
-    Extract key information about bacteria from a given dataframe for a specific time step.
+def checking_columns(data_frame):
+    center_coordinate_columns = []
 
-    @param bac_in_timestep DataFrame containing bacteria information for a particular time step.
+    parent_image_number_col = [col for col in data_frame.columns if 'TrackObjects_ParentImageNumber_' in col][0]
+    parent_object_number_col = [col for col in data_frame.columns if 'TrackObjects_ParentObjectNumber_' in col][0]
+
+    if 'Location_Center_X' in data_frame.columns and 'Location_Center_Y' in data_frame.columns:
+        center_coordinate_columns = {'x': 'Location_Center_X', 'y': 'Location_Center_Y'}
+
+    elif 'AreaShape_Center_X' in data_frame.columns and 'AreaShape_Center_Y' in data_frame.columns:
+        center_coordinate_columns = {'x': 'AreaShape_Center_X', 'y': 'AreaShape_Center_Y'}
+
+    else:
+        print('There was no column corresponding to the center of bacteria.')
+        breakpoint()
+
+    return center_coordinate_columns, parent_image_number_col, parent_object_number_col
+
+
+def bac_info(bac_in_timestep, center_coordinate_columns):
+    """
+    Extract key information about bacteria from a given DataFrame for a specific time step.
+
+    @param bac_in_timestep DataFrame DataFrame containing bacteria information for a particular time step.
+    @param center_coordinate_columns dict column name of center_x & center_y
 
     Returns:
     tuple: A tuple containing four elements:
@@ -32,18 +55,15 @@ def bac_info(bac_in_timestep):
            - Objects_major_axis (Series): The lengths of the major axes of the bacteria.
            - Objects_orientation (Series): The orientations of the bacteria.
     """
-    # center coordinate
-    try:
-        Objects_center_x = bac_in_timestep["Location_Center_X"]
-        Objects_center_y = bac_in_timestep["Location_Center_Y"]
-    except KeyError:
-        Objects_center_x = bac_in_timestep["AreaShape_Center_X"]
-        Objects_center_y = bac_in_timestep["AreaShape_Center_Y"]
 
-    # major axis length
+    # Calculate the center coordinates of bacteria in micrometers
+    Objects_center_x = bac_in_timestep[center_coordinate_columns['x']]
+    Objects_center_y = bac_in_timestep[center_coordinate_columns['y']]
+
+    # Calculate the major axis length of bacteria in micrometers
     Objects_major_axis = bac_in_timestep["AreaShape_MajorAxisLength"]
 
-    # orientation
+    # Get the orientation of bacteria in radians
     Objects_orientation = bac_in_timestep["AreaShape_Orientation"]
 
     return Objects_center_x, Objects_center_y, Objects_major_axis, Objects_orientation
@@ -57,26 +77,32 @@ def find_vertex(center_x, center_y, major, angle_rotation, angle_tolerance=1e-6)
     @param center_y float The y-coordinate of the ellipse's center.
     @param major float The length of the major axis of the ellipse.
     @param angle_rotation float The angle of rotation of the ellipse from the horizontal axis in radians.
-    @param angle_tolerance float(optional) A small tolerance value used to handle floating-point errors.
-    Default is 1e-6.
+    @param angle_tolerance float A small tolerance value used to handle floating-point errors. Default is 1e-6.
 
     Returns:
     list: A list containing two lists, each representing the x and y coordinates of a vertex.
+
+    Notes:
+    The function checks for two special cases:
+    1. When the ellipse is nearly parallel to the vertical axis (angle_rotation ≈ π/2).
+    2. When the ellipse is nearly parallel to the horizontal axis (angle_rotation ≈ 0).
+    For these cases, the vertices are calculated differently to avoid numerical instability.
     """
-    # Special case: Ellipse is nearly vertical
-    if np.abs(np.abs(angle_rotation) - np.pi / 2) < angle_tolerance:  # Bacteria parallel to the vertical axis
+
+    # Handle cases where the ellipse is nearly vertical
+    if np.abs(np.abs(angle_rotation) - np.pi / 2) < angle_tolerance:
         vertex_1_x = center_x
         vertex_1_y = center_y + major / 2
         vertex_2_x = center_x
         vertex_2_y = center_y - major / 2
-    # Special case: Ellipse is nearly horizontal
-    elif np.abs(angle_rotation) < angle_tolerance:  # Bacteria parallel to the horizontal axis
+    # Handle cases where the ellipse is nearly horizontal
+    elif np.abs(angle_rotation) < angle_tolerance:
         vertex_1_x = center_x + major / 2
         vertex_1_y = center_y
         vertex_2_x = center_x - major / 2
         vertex_2_y = center_y
     else:
-        # General case: Ellipse at an arbitrary angle
+        # General case for any arbitrary angle
         semi_major = major
         vertex_1_x = float(semi_major / (np.cos(angle_rotation) + np.tan(angle_rotation) * np.sin(angle_rotation)) +
                            center_x)
@@ -88,135 +114,167 @@ def find_vertex(center_x, center_y, major, angle_rotation, angle_tolerance=1e-6)
     return [[vertex_1_x, vertex_1_y], [vertex_2_x, vertex_2_y]]
 
 
-def lineage_life_history_plot(df_current, img_dir, TimeStep, axis, clr, prefix_raw_name, font_size, postfix_raw_name):
-    """
-    Plot the lineage life history of cells in a given time step on a background image.
+def draw_tracking_plot(df_current, raw_images, timestep, axis, object_color, font_size, center_coordinate_columns,
+                       parent_image_number_col, parent_object_number_col):
 
-    @param df_current DataFrame containing cell information.
-    @param img_dir str Directory path where the background images are stored.
-    @param TimeStep int The current time step for which the plot is generated.
-    @param axis matplotlib.axes.Axes The matplotlib axis on which to plot.
-    @param clr str Color used for plotting the cells.
-    @param prefix_raw_name str Prefix for the raw image files.
-    @param font_size int Font size for the cell ID annotations.
-    @param postfix_raw_name str Postfix for the raw image files.
     """
-    # draw Objects
-    TimeStep = int(TimeStep)
+    Plot the lineage life history of bacteria in a given time step on a background image (raw image).
+
+    @param df_current DataFrame DataFrame containing cell information for the current timestep.
+    @param raw_images list List of paths to raw images for each timestep.
+    @param timestep int The current time step for which the plot is generated.
+    @param axis matplotlib.axes.Axes The matplotlib axis on which to plot.
+    @param object_color str Color used for plotting the bacteria.
+    @param font_size float Font size for labeling the cells.
+    @param center_coordinate_columns dict column name of center_x & center_y
+    @param parent_image_number_col str column name of parent image number
+    @param parent_object_number_col str column name of parent object number
+    """
+
+    timestep = int(timestep)
     ax = axis
 
-    # Adjust image file name format based on the length of TimeStep string
-    if len(str(TimeStep - 1)) == 1:
-        img_name = prefix_raw_name + "0" + str(TimeStep - 1) + postfix_raw_name
-    elif len(str(TimeStep)) == 2:
-        img_name = prefix_raw_name + "" + str(TimeStep - 1) + postfix_raw_name
-    elif len(str(TimeStep)) == 3:
-        img_name = prefix_raw_name + "" + str(TimeStep - 1) + postfix_raw_name
+    # Get the raw image for the current time step
+    current_raw_img = raw_images[timestep - 1]
+    print('Raw image path for this time step: ' + str(current_raw_img))
 
     # Read and display the background image
-    img = cv2.imread(img_dir + '/' + img_name)
+    img = cv2.imread(current_raw_img)
     plt.imshow(img)
     ax.imshow(img)
 
     # Extract objects' information from the current time step
-    (Objects_center_coord_x, Objects_center_coord_y, Objects_major_current, Objects_orientation_current) = \
-        bac_info(df_current)
+    Objects_center_coord_x, Objects_center_coord_y, Objects_major_current, Objects_orientation_current = \
+        bac_info(df_current, center_coordinate_columns)
 
     # Plot each cell
     num_cells = df_current.shape[0]
     for cell_indx in range(num_cells):
         # Get the current cell's information
         center_current = (Objects_center_coord_x[cell_indx], Objects_center_coord_y[cell_indx])
-        major_current = (Objects_major_current.iloc[cell_indx]) / 2
-        # radian
+        major_current = (Objects_major_current.iloc[cell_indx]) / 2  # Half the length for plotting
+
+        # Get the orientation in radians
         angle_current = Objects_orientation_current.iloc[cell_indx]
 
         # Calculate the endpoints of the cell
         ends = find_vertex(center_current[0], center_current[1], major_current, angle_current)
-        # endpoints
+        # Endpoints of the major axis of the ellipse
         node_x1_x_current = ends[0][0]
         node_x1_y_current = ends[0][1]
         node_x2_x_current = ends[1][0]
         node_x2_y_current = ends[1][1]
 
-        # plot current bac
+        # Plot current bacteria
         ax.plot([node_x1_x_current, node_x2_x_current], [node_x1_y_current, node_x2_y_current], lw=1,
-                solid_capstyle="round", color=clr)
+                solid_capstyle="round", color=object_color)
 
         # Add cell ID and parent ID as text
         pos1x = np.abs(node_x1_x_current + center_current[0]) / 2
+        pos2x = np.abs(node_x2_x_current + center_current[0]) / 2
+
         pos1y = np.abs(node_x1_y_current + center_current[1]) / 2
+        pos2y = np.abs(node_x2_y_current + center_current[1]) / 2
+
         final_pos1x = np.abs(pos1x + center_current[0]) / 2
+        final_pos2x = np.abs(pos2x + center_current[0]) / 2
+
         final_pos1y = np.abs(pos1y + center_current[1]) / 2
+        final_pos2y = np.abs(pos2y + center_current[1]) / 2
 
-        ax.text(final_pos1x, final_pos1y, int(df_current.iloc[cell_indx]["ObjectNumber"]), fontsize=font_size,
-                color="#ff0000")
+        # Plot cell ID and parent ID
+        ax.text(final_pos1x, final_pos1y, int(df_current.iloc[cell_indx]["ObjectNumber"]), fontsize=font_size, color="#ff0000")
+        ax.text(final_pos2x, final_pos2y, int(df_current.iloc[cell_indx][parent_object_number_col]), fontsize=font_size,
+                color="#0000ff")
 
 
-def tracking_bac(raw_img, csv_file_path, output_dir, color, prefix_raw_name, font_size, postfix_raw_name):
+def tracking_bac(raw_img_dir, cellprofiler_csv_output_file, output_dir, object_color, font_size):
     """
-    Generate tracking plots for bacteria over different time steps using data from CSV files.
+    Generate tracking plots for bacteria over different time steps using data from CSV file.
 
-    @param raw_img str Directory where the raw images are stored.
-    @param csv_file_path str Path to the CSV file containing bacteria data.
+    @param raw_img_dir str Directory path where the raw images are stored.
+    @param cellprofiler_csv_output_file str Path to the CSV file output of Cellprofiler.
     @param output_dir str Directory where the output plots should be saved.
-    @param color str Color used for plotting.
-    @param prefix_raw_name str Prefix for the raw image files.
-    @param font_size int Font size for annotations.
-    @param postfix_raw_name str Postfix for the raw image files.
+    @param object_color str Color of objects in tracking plots.
+    @param font_size float Font size for labeling the cells in the plots.
     """
-    # read cp output
-    df = pd.read_csv(csv_file_path)
-    modified_df = angle_convert_to_radian(df)
 
-    # time steps
-    t = list(set(modified_df['ImageNumber'].values))
-    num_digit = len(str(t[-1]))
+    # Read CSV file containing tracking data
+    df = pd.read_csv(cellprofiler_csv_output_file)
+    df = angle_convert_to_radian(df)
+
+    center_coordinate_columns, parent_image_number_col, parent_object_number_col = checking_columns(df)
+
+    # Get list of all raw images
+    raw_images = glob.glob(raw_img_dir + '/*.tif')
+
+    # Get unique time steps
+    t = np.unique(df['ImageNumber'].values)
+    num_digit = len(str(t[-1]))  # To format filenames consistently
+
+    # Create output directory if not provided
+    if output_dir is None:
+        output_dir = os.path.dirname(cellprofiler_csv_output_file) + '/Cellprofiler_tracking_plot/'
 
     os.makedirs(output_dir, exist_ok=True)
 
     for timestep in t:
-        print('Time step ' + str(timestep))
-        df_current = modified_df.loc[modified_df["ImageNumber"] == timestep]
+        print('Time Step: ' + str(timestep))
+
+        # Get data for the current timestep
+        df_current = df.loc[df["ImageNumber"] == timestep]
         df_current = df_current.reset_index(drop=True)
 
-        # plot
+        # Plot tracking plot for the current timestep
         fig, ax = plt.subplots()
-        lineage_life_history_plot(df_current, raw_img, timestep, ax, color, prefix_raw_name, font_size,
-                                  postfix_raw_name)
-        plt.suptitle("Objects in time step = " + str(timestep), fontsize=14, fontweight="bold")
+        draw_tracking_plot(df_current, raw_images, timestep, ax, object_color, font_size, center_coordinate_columns,
+                           parent_image_number_col, parent_object_number_col)
 
-        obj_num_patch = mpatches.Patch(color='#ff0000', label='ObjectNumber')
-        plt.legend(handles=[obj_num_patch], loc='upper right', ncol=6, bbox_to_anchor=(.75, 1.07), prop={'size': 7})
+        # Add legend and title
+        plt.suptitle("Cellprofiler: Tracking objects in time step = " + str(timestep), fontsize=14, fontweight="bold")
+        parent_patch = mpatches.Patch(color='#0000ff', label='Parent object number', )
+        bacteria_patch = mpatches.Patch(color='#ff0000', label='Bacteria object number', )
+        plt.legend(handles=[parent_patch, bacteria_patch], loc='upper right', ncol=6,
+                   bbox_to_anchor=(.75, 1.07), prop={'size': 7})
 
-        fig.savefig(output_dir + '/' + "timeStep_t_" +
+        # Save the figure
+        fig.savefig(output_dir + '/' + "Cellprofiler.timeStep t = " +
                     '0' * (num_digit - len(str(timestep))) + str(timestep) + ".png", dpi=600)
+        # Close the figure
         fig.clf()
         plt.close()
 
 
 if __name__ == "__main__":
-    # Define the paths and parameters
-    # Path to the CSV file containing bacteria data.
-    cp_output_csv_file = 'CP_output/FilterObjects.csv'
 
-    # Directory where the raw images are stored.
-    raw_img_dir = 'modified_images/'
+    # Initialize the ArgumentParser object
+    parser = argparse.ArgumentParser(description='Generate tracking plots for cell lineages using Cellprofiler output.')
 
-    # Directory where the output plots will be saved.
-    output_dir = 'tracking_plot2/'
+    # Add arguments for command line interface
+    parser.add_argument('-t', '--cellprofiler_csv_output_file', help='CSV file output of Cellprofiler.')
 
-    # Prefix for the raw image files.
-    prefix_raw_name = 'K12_Scene2_C0_T'
+    parser.add_argument('-r', '--raw_image_dir', help='Directory containing raw images. '
+                                                      'The format of raw images should be tif.')
 
-    # Postfix for the raw image files.
-    postfix_raw_name = '.tif'
+    parser.add_argument('-o', '--output', default=None,
+                        help="Directory where to save tracking plots. "
+                             "Default value: Save to the Cellprofiler output file folder.")
 
-    # Color used for plotting.
-    color = '#56e64e'
-    
-    # Font size for annotations.
-    font_size = 2
+    parser.add_argument('-c', '--objectColor', default="#56e64e",
+                        help="Color of objects in tracking plots. Default value: #56e64e.")
 
-    # Run the bacteria tracking plot generation
-    tracking_bac(raw_img_dir, cp_output_csv_file, output_dir, color, prefix_raw_name, font_size, postfix_raw_name)
+    parser.add_argument('-f', '--font_size', default="1",
+                        help="Font size for labeling information on objects. Default value: 1")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Assign parsed arguments to variables
+    raw_img_dir = args.raw_image_dir
+    cellprofiler_csv_output_file = args.cellprofiler_csv_output_file
+    output_dir = args.output
+    object_color = args.objectColor
+    font_size = float(args.font_size)
+
+    # Generate tracking plots for bacteria
+    tracking_bac(raw_img_dir, cellprofiler_csv_output_file, output_dir, object_color, font_size)
