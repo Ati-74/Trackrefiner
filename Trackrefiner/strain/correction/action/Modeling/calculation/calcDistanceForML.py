332e3a83bf99c491a0c09723067c5cb2dbeec1f6
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2024-09-10 16:35:39 +0200

    minor changes

--- Trackrefiner/strain/correction/action/Modeling/calculation/calcDistanceForML.py
+++ Trackrefiner/strain/correction/action/Modeling/calculation/calcDistanceForML.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def calc_distance(df, center_coordinate_columns, postfix_source, postfix_target, stat=None):
@@ -55,3 +56,61 @@ def calc_distance(df, center_coordinate_columns, postfix_source, postfix_target,
         df['min_distance_same'] = df[dis_col_same].min(axis=1)
 
     return df
+
+
+def new_calc_distance(df, center_coordinate_columns, postfix_source, postfix_target, stat=None):
+
+    distance_df = pd.DataFrame(index=df.index)
+
+    dis_cols = ['center_distance', 'endpoint1_1_distance', 'endpoint2_2_distance']
+    dis_col_same = dis_cols.copy()
+
+    # create distance matrix (row: parent bacterium, column: candidate daughters in next time step)
+    distance_df['center_distance'] = \
+        np.linalg.norm(
+            df[[center_coordinate_columns['x'] + postfix_source,
+                center_coordinate_columns['y'] + postfix_source]].values -
+            df[[center_coordinate_columns['x'] + postfix_target,
+                center_coordinate_columns['y'] + postfix_target]].values,
+            axis=1)
+
+    distance_df['endpoint1_1_distance'] = \
+        np.linalg.norm(df[['endpoint1_X' + postfix_source, 'endpoint1_Y' + postfix_source]].values -
+                       df[['endpoint1_X' + postfix_target, 'endpoint1_Y' + postfix_target]].values, axis=1)
+
+    distance_df['endpoint2_2_distance'] = \
+        np.linalg.norm(df[['endpoint2_X' + postfix_source, 'endpoint2_Y' + postfix_source]].values -
+                       df[['endpoint2_X' + postfix_target, 'endpoint2_Y' + postfix_target]].values, axis=1)
+
+    if stat == 'div':
+
+        dis_cols.extend(['endpoint1_2_distance', 'endpoint2_1_distance', 'center_endpoint1_distance',
+                         'center_endpoint2_distance'])
+
+        distance_df['endpoint1_2_distance'] = \
+            np.linalg.norm(df[['endpoint1_X' + postfix_source, 'endpoint1_Y' + postfix_source]].values -
+                           df[['endpoint2_X' + postfix_target, 'endpoint2_Y' + postfix_target]].values,
+                           axis=1)
+
+        distance_df['endpoint2_1_distance'] = \
+            np.linalg.norm(df[['endpoint2_X' + postfix_source, 'endpoint2_Y' + postfix_source]].values -
+                           df[['endpoint1_X' + postfix_target, 'endpoint1_Y' + postfix_target]].values,
+                           axis=1)
+
+        distance_df['center_endpoint1_distance'] = \
+            np.linalg.norm(df[[center_coordinate_columns['x'] + postfix_source,
+                               center_coordinate_columns['y'] + postfix_source]].values -
+                           df[['endpoint1_X' + postfix_target, 'endpoint1_Y' + postfix_target]].values,
+                           axis=1)
+
+        distance_df['center_endpoint2_distance'] = \
+            np.linalg.norm(df[[center_coordinate_columns['x'] + postfix_source,
+                               center_coordinate_columns['y'] + postfix_source]].values -
+                           df[['endpoint2_X' + postfix_target, 'endpoint2_Y' + postfix_target]].values, axis=1)
+
+    distance_df['min_distance'] = distance_df[dis_cols].min(axis=1)
+
+    if stat == 'div':
+        distance_df['min_distance_same'] = distance_df[dis_col_same].min(axis=1)
+
+    return distance_df
--- Trackrefiner/strain/correction/action/Modeling/calculation/iouCalForML.py
+++ Trackrefiner/strain/correction/action/Modeling/calculation/iouCalForML.py
@@ -1,3 +1,44 @@
+import pandas as pd
+
+
+def new_calculate_intersections_and_unions(df, col1, col2, stat='same'):
+
+    df_iou = pd.DataFrame(index=df.index)
+
+    if stat == 'same':
+        df_iou['intersection'] = df[col1].values & df[col2].values
+        df_iou['union'] = df[col1].values | df[col2].values
+
+        df_iou['intersection'] = df_iou['intersection'].map(len)
+        df_iou['union'] = df_iou['union'].map(len)
+
+        df_iou['intersection'] = df_iou['intersection'].astype('int64')
+        df_iou['union'] = df_iou['union'].astype('int64')
+
+        df_iou['iou'] = df_iou['intersection'] / df_iou['union']
+
+    elif stat == 'div':
+
+        df_iou['intersection'] = df[col1].values & df[col2].values
+        df_iou['intersection'] = df_iou['intersection'].map(len)
+
+        df_iou['union'] = df[col1].values | df[col2].values
+        df_iou['union'] = df_iou['union'].map(len)
+
+        # Calculate unique areas for mask 2 (daughter)
+        df_iou['unique_masks2'] = df[col2] - df[col1]
+        df_iou['unique_masks2'] = df_iou['unique_masks2'].map(len)
+
+        df_iou['intersection'] = df_iou['intersection'].astype('int64')
+        df_iou['unique_masks2'] = df_iou['unique_masks2'].astype('int64')
+        df_iou['union'] = df_iou['union'].astype('int64')
+
+        # Calculate IoU based on daughter_flag
+        df_iou['iou'] = df_iou['intersection'] / (df_iou['intersection'] + df_iou['unique_masks2'])
+        df_iou['iou_same'] = df_iou['intersection'] / df_iou['union']
+
+    return df
+
 
 def calculate_intersections_and_unions(df, col1, col2, stat='same'):
 
@@ -14,6 +55,7 @@ def calculate_intersections_and_unions(df, col1, col2, stat='same'):
         df['iou'] = df['intersection'] / df['union']
 
     elif stat == 'div':
+
         df['intersection'] = df[col1].values & df[col2].values
         df['intersection'] = df['intersection'].map(len)
 
@@ -31,6 +73,18 @@ def calculate_intersections_and_unions(df, col1, col2, stat='same'):
         # Calculate IoU based on daughter_flag
         df['iou'] = df['intersection'] / (df['intersection'] + df['unique_masks2'])
         df['iou_same'] = df['intersection'] / df['union']
+
+    return df
+
+
+def new_iou_calc(df, stat, col_source='coordinate_prev', col_target='coordinate'):
+
+    df = new_calculate_intersections_and_unions(df, col1=col_source, col2=col_target, stat=stat)
+
+    df['iou'] = 1 - df['iou']
+
+    if stat == 'div':
+        df['iou_same'] = 1 - df['iou_same']
 
     return df
 
