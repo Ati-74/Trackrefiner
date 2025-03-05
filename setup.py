c6621fd2341671d8de325dfc486d25f510d9aa63
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2025-03-05 21:06:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='TrackRefiner',
-      version='1.2.4',
+      version='1.2.5',
       description='A precision tracking tool for bacillus cell tracking',
       long_description=long_description,
       long_description_content_type="text/markdown",
